9992892ec68c0794566f5dda292d589e403e39bc
Pedro Estevão <61397405+Pedro-Estevao@users.noreply.github.com>
2023-08-05 17:26:53 -0300

    Configurações iniciais #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import Head from "next/head";
 import ThemeToggle from '@/components/ThemeToggle';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-dark">
-			<ThemeToggle />
+		<>
+			<Head>
+				<title>Boilerplate - NextJS</title>
+			</Head>
 
-			<p className="text-black dark:text-white">Apenas um teste</p>
-		</main>
+			<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-dark">
+				<h1 className="text-black dark:text-white">
+					Welcome to <a href="https://nextjs.org">Next.js!</a>
+				</h1>
+
+				<ThemeToggle />
+
+				<p className="text-black dark:text-white">Apenas um teste</p>
+			</main>
+		</>
 	);
 };
---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	dir: './',
+});
+
+const config = {
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	testEnvironment: 'jest-environment-jsdom',
+	testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+	collectCoverage: true,
+	coverageProvider: 'v8',
+	collectCoverageFrom: [
+		'**/*.{js,jsx,ts,tsx}',
+		'!**/*.d.ts',
+		'!**/node_modules/**',
+		'!<rootDir>/out/**',
+		'!<rootDir>/.next/**',
+		'!<rootDir>/*.config.js',
+		'!<rootDir>/coverage/**',
+	],
+	moduleNameMapper: {
+		'^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+		'^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+		'^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+		'^@/components/(.*)$': '<rootDir>/components/$1',
+	},
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+	},
+	transformIgnorePatterns: [
+		'/node_modules/',
+		'^.+\\.module\\.(css|sass|scss)$',
+	],
+};
+
+module.exports = createJestConfig(config);
